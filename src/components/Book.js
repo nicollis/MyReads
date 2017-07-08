42e5af3764ae3f2091893989b74c054c31fd980d
Nic Ollis <nic@ollis.me>
2017-07-08 13:59:10 -0400

    feat: Making bookshelf dynamic
    Created Bookshelf componenet
    Created pull from database to get all books and fill shelfs based on filter

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,9 +5,9 @@ class Book extends Component {
   static propTypes = {
     Book: PropTypes.shape({
       id: PropTypes.string,
-      url: PropTypes.string.isRequired,
+      imageLinks: PropTypes.object.isRequired,
       title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
+      authors: PropTypes.array.isRequired,
       shelf: PropTypes.string.isRequired,
     }),
     onChangeSelf: PropTypes.func.isRequired
@@ -16,22 +16,26 @@ class Book extends Component {
   render () {
     const { Book } = this.props;
     return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${Book.url})`}}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${Book.imageLinks.thumbnail})`}}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
           </div>
+          <div className="book-title">{Book.title}</div>
+          {Book.authors.map((author) => (
+            <div key={author} className="book-authors">{author}</div>
+          ))}
         </div>
-        <div className="book-title">{Book.title}</div>
-        <div className="book-authors">{Book.author}</div>
-      </div>
+      </li>
     )
   }
 }
